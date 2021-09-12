ded785cb9b60a150558cbf6f4010a11d76c56ec1
Maxim-Cherkasov <84859924+Maxim-Cherkasov@users.noreply.github.com>
2021-09-12 10:51:14 +0300

    for while added

--- js-if/js/script.js
+++ js-if/js/script.js
@@ -3,7 +3,7 @@
 1. Изучить теорию
 2. Какой или какие из вариантов не верны:
 */
-
+/*
 //Вариант №1
 if (1 === "1") {
 	console.log('Истина!');
@@ -27,7 +27,7 @@ if (0) {
 } else if (" ") {
 	console.log('Истина!');
 }
-
+*/
 //========================================================================================================================================================
 //========================================================================================================================================================
 /*
@@ -154,4 +154,140 @@ messageEnd = 5 > 10 ? ", Vasia" :
 message += messageEnd;
 console.log(message);
 */
+
+/*
+let year = prompt('In which year was ECMAScript-2015 specification published?', '');
+
+if (year == 2015) {
+  alert( 'You guessed it right!' );
+} else {
+  alert( 'How can you be so wrong?' ); // any value except 2015
+}
+*/
+
+/*
+let year = prompt('In which year was the ECMAScript-2015 specification published?', '');
+
+if (year < 2015) {
+  alert( 'Too early...' );
+} else if (year > 2015) {
+  alert( 'Too late' );
+} else {
+  alert( 'Exactly!' );
+}
+*/
+
+/*
+let accessAllowed;
+let age = prompt('How old are you?', '');
+
+if (age > 18) {
+  accessAllowed = true;
+} else {
+  accessAllowed = false;
+}
+
+alert(accessAllowed);
+*/
+
+/*
+let accessAllowed;
+let age = prompt('How old are you', '');
+
+if (age > 18) {
+	accessAllowed = true;
+} else {
+	accessAllowed = false;
+}
+
+alert(accessAllowed);
+*/
+
+/*
+let accessAllowed = (age > 18) ? true : false;
+*/
+
+/*
+let age = prompt('age?', 18);
+
+let message = (age < 3) ? 'Hi, baby!' :
+  (age < 18) ? 'Hello!' :
+  (age < 100) ? 'Greetings!' :
+  'What an unusual age!';
+
+alert( message );
+*/
+/*
+let age = prompt('What is your age?', 18);
+
+let message = (age < 3) ? 'Hi, baby!' :
+	(age < 18) ? 'Hello!' :
+	(age > 18) ? 'Greetings!' :
+	'What an unusual age!';
+
+	alert( message );
+	*/
+
+/*
+let company = prompt('Which company created JavaScript?', '');
+
+if (company == 'Netscape') {
+  alert('Right!');
+} else {
+  alert('Wrong.');
+}	
+*/
+
+/*
+let company  = prompt('Which company created JavaScript', '');
+
+if (company == 'Netscape') {
+	alert('Right!');
+} else {
+	alert('Wrong!');
+}
+*/
+
+/*
+let jsName = prompt('What is the oficial name of JS', '');
+
+if (jsName == 'Ecma') {
+	alert('Right');
+} else {
+	alert('Wrong');
+}
+*/
+
+/*
+let result;
+
+if (a + b < 4) {
+  result = 'Below';
+} else {
+  result = 'Over';
+}
+*/
+
+/*
+let result = prompt(a + b < 4) ? Below : Over;
+*/
+
+/*
+let message;
+
+if (login == 'Employee') {
+  message = 'Hello';
+} else if (login == 'Director') {
+  message = 'Greetings';
+} else if (login == '') {
+  message = 'No login';
+} else {
+  message = '';
+}
+*/
+
+let message = (login == 'Employee') ? 'Hello' :
+  (login == 'Director') ? 'Greetings' :
+  (login == '') ? 'No login' :
+  '';
 
