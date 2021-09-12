071c0f7e2a01465c96ff5061f7483e7f851aa888
Maxim-Cherkasov <84859924+Maxim-Cherkasov@users.noreply.github.com>
2021-09-12 13:11:41 +0300

    2

--- js-if/js/script.js
+++ js-if/js/script.js
@@ -285,9 +285,9 @@ if (login == 'Employee') {
   message = '';
 }
 */
-
+/*
 let message = (login == 'Employee') ? 'Hello' :
   (login == 'Director') ? 'Greetings' :
   (login == '') ? 'No login' :
   '';
-
+*/
